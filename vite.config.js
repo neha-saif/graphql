4b2f8eec3e21ac9ca0c75b0b736617e6df8c4756
neali <nehasaif@msn.com>
2025-11-26 14:16:50 +0300

    .

--- vite.config.js
+++ vite.config.js
@@ -3,14 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      "/graphql": {
-        target: "https://learn.reboot01.com/api/graphql-engine/v1",
-        changeOrigin: true,
-        secure: false,
-        rewrite: (path) => path.replace(/^\/graphql/, "")
-      }
-    }
-  }
+  base: "/graphql/",   
 });
