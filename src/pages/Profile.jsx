b85c43f67c1b4e58e08d42abbd58ecfb83101c76
neali <nehasaif@msn.com>
2025-11-27 15:12:50 +0300

    .

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -2,7 +2,7 @@ import { useEffect, useMemo, useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 import { getToken, clearToken } from "../lib/auth.js";
-import { gqlFetch } from "../graphql.js";
+import { gqlFetch } from "../api.js";
 import ProfileSummary from "./ProfileSummary.jsx";
 
 import {
