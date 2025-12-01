4d01c028b74b81e175a7a5e61f2f90ba1d209519
neali <nehasaif@msn.com>
2025-12-01 18:45:13 +0300

    .

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,8 @@
-import React from "react";
-import { createRoot } from "react-dom/client";
-import { HashRouter } from "react-router-dom";
-import App from "./App.jsx";
-import "./index.css";
 import { BrowserRouter } from "react-router-dom";
-
+import App from "./App.jsx";
 
 createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
+  <BrowserRouter basename="/graphql">
     <App />
   </BrowserRouter>
 );
