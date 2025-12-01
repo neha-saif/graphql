e1bcd9a8de78f8e6d075002f4a00dd2cbc7f047d
neali <nehasaif@msn.com>
2025-12-01 18:40:26 +0300

    .

--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,11 @@ import { createRoot } from "react-dom/client";
 import { HashRouter } from "react-router-dom";
 import App from "./App.jsx";
 import "./index.css";
+import { BrowserRouter } from "react-router-dom";
+
 
 createRoot(document.getElementById("root")).render(
-<>
-<HashRouter>
-  <App />
-</HashRouter>
-
-  </>
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
 );
