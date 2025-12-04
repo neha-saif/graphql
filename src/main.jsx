44c12688d610d864278635ac98810d86d90fd037
neali <nehasaif@msn.com>
2025-12-04 16:44:27 +0300

    .

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import App from "./App.jsx";
 import "./index.css";
 
 const container = document.getElementById("root");
 
 createRoot(container).render(
-  <BrowserRouter basename="/graphql">
+  <HashRouter>
     <App />
-  </BrowserRouter>
+  </HashRouter>
 );
